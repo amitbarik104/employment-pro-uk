3503cf9ca80ede74b838f77d51e47617d619ce8f
amitbarik104 <amitbarik104@gmail.com>
2023-04-27 21:45:26 +0100

    added print file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Print from './Print';
 
 const App = () => {
-  return <div>In APP.js file</div>
+  return <div>In APP.js file
+
+    <Print />
+  </div>
 }
 
 export default App;
---
+++ client/src/Print.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Print = () => <span>Just another file to print the message</span>
+
+
+export default Print
